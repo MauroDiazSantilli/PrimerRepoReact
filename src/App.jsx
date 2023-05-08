4468fd3b4f0c27298328abe4b65a8667d399f880
MauroDS <mauro.diaz.santilli@gmail.com>
2023-05-08 11:34:02 -0300

    Instalando clases bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import './App.css'
 import Titulos from "./components/Titulos"
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
 //Aqui escribimos logica
 
   return (
   //Aqui va el maquetado y un poco de logica - //El simbolo <> es "fragment""
-  <>   
+  <section className='container my-4'>   
      <Titulos></Titulos>
-    </>
+    </section>
 
   )
 }
