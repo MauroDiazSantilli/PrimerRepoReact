0b3cfd5d03ec5d9d3f320e511cd52fa0b5546b14
MauroDS <mauro.diaz.santilli@gmail.com>
2023-05-08 10:40:15 -0300

    Limpiando el codigo incial

--- src/App.jsx
+++ src/App.jsx
@@ -7,12 +7,9 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
+    
       <div></div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+
   )
 }
 
